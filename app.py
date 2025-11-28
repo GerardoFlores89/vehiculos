a57616e33a6656d6ff667a7548c54332d6952d7c
Gerardo Flores <jgfn891108@gmail.com>
2025-11-28 14:49:14 -0600

    Creación de archivos

---
+++ app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv')
+
+st.header('Aplicación de Vehiculos')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
