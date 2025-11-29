7f95fde66701c8a1cce0f54d63aeba8c5e3c2cf0
Gerardo Flores <jgfn891108@gmail.com>
2025-11-29 10:17:12 -0600

    Creacion de boton Sactter

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ car_data = pd.read_csv('vehicles_us.csv')  # lectura del archivo
 st.markdown("<h1 style='text-align: center;'>Vehiculos</h1>",
             unsafe_allow_html=True)
 
-hist_button = st.button('Construir histograma')
+histogram_button = st.button('Construir histograma')
 
-if hist_button:
+if histogram_button:
     st.write(
         'Creación de un histograma para la visualización de los precios')
     fig = px.histogram(car_data, x='price',
@@ -18,11 +18,26 @@ if hist_button:
                        title='Distribución de precio de los vehiculos de acuerdo a su condición')
     fig.update_layout(
         title_x=0.2,
-        xaxis_title='Precio en USD',
+        xaxis_title='Precio ($)',
         yaxis_title='Número de Vehículos',
         xaxis=dict(dtick=30000),
         yaxis=dict(dtick=400),
         width=800,
         height=600
+    )
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Gráfico de dispersión')
+
+if scatter_button:
+    st.write(
+        'Creación de un gráfico de dispersión para analizar la relación entre odómetro y precio')
+    fig = px.scatter(car_data, x='odometer', y='price',
+                     color='condition',
+                     title='Relación entre Odómetro y Precio de acuerdo a su condición')
+    fig.update_layout(
+        title_x=0.2,
+        xaxis_title='Odómetro (millas)',
+        yaxis_title='Precio ($)'
     )
     st.plotly_chart(fig, use_container_width=True)
